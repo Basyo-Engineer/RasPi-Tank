bf074a7b6fc51a2447c775e3a9ef2d98dbca9650
Basyo-Engineer <svarval.lite@gmail.com>
2024-02-29 15:31:43 +0900

    Recode

--- main.py
+++ main.py
@@ -49,12 +49,6 @@ def Back(msec):
 servo1 = PWM(Pin(7))
 servo1.freq(50)
 
-max_duty = 65025
-dig_min = 0.025   #-90°
-dig_max = 0.12     #90°
-
-i = 0
-status = 0
 
 while True :
     trig.low()
@@ -73,33 +67,10 @@ while True :
     time.sleep(0.1)
     
     if distance <= 10:
-        Back(2)
-        TurnR(0.1)
+        Back(1)
+        TurnR(0.5)
         
-        while distance <= 10:
-            if i > 180:
-                status = 1
-            elif i < 0:
-                status = 0
-            deg = dig_min + i * (dig_max - dig_min) / 180
-            print(i)
-            if status > 0:
-                i -= 1
-            else:
-                i += 1
-            servo1.duty_u16(int(deg * max_duty))
-            time.sleep_ms(10)
-            
-            if distance > 10:
-                status = 0
-                break
-            
-        
-        
-        
-        
-        
-        
+
     else:
         Forword(0.1)
    
